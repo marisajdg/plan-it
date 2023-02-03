4be6b8446d5f04c15ffa43e59bea754961ddc14d
jc-juarez <jc.juarezgarcia@outlook.com>
2023-02-02 22:55:35 -0600

    Corrected app.py

--- src/app.py
+++ src/app.py
@@ -10,4 +10,4 @@ from flask import Flask
 app = Flask(__name__)
 
 if __name__ == '__main__':
-    app.run(host=constants.'0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
